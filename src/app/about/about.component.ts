c6c9ef9cf37c82fcd180b2e5bfaf00b09d00f82f
Josue Morales <dev.carlos.morales@gmail.com>
2025-05-01 12:40:25 -0500

    fix: update AboutComponent template to reflect new title

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  template: ` <h1>New About Name</h1>`,
+  template: ` <h1>New About Name 2</h1>`,
 })
 export default class AboutComponent {}
